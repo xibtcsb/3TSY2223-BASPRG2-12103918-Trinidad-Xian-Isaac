3f1a224bf3a16749e0268ac8f2781431ad574db8
xibtcsb <xianisaac.trinidad@benilde.edu.ph>
2023-06-24 16:31:09 +0800

    Update quiz1_TRINIDAD.cpp
    updated quiz 1 as of 6/24

--- quiz1_TRINIDAD.cpp
+++ quiz1_TRINIDAD.cpp
@@ -4,62 +4,92 @@
 
 using namespace std;
 
-void playRound(int round, int& mmLeft, int& moneyEarned) {
-    // Determine Kaiji's side for the current round
+string kaijiSide;
+
+void playRound(int round, int& mmLeft, int& moneyEarned, int wager) {
     string kaijiSide;
-    if (round % 4 == 0 || round % 4 == 1 || round % 4 == 2) {
+    if (round <= 3 || round == 7 || round == 8 || round == 9) {
         kaijiSide = "Emperor";
     }
     else {
         kaijiSide = "Slave";
     }
 
-    // Generate opponent's card randomly
     string opponentCard;
-    int randomNum = rand() % 3;
-    if (randomNum == 0) {
-        opponentCard = "Civilian";
+    if (round <= 3 || (round >= 10 && round <= 12)) {
+        opponentCard = "Citizen";
     }
-    else if (randomNum == 1) {
-        opponentCard = "Slave";
-    }
-    else {
+    else if (round >= 4 && round <= 6) {
         opponentCard = "Emperor";
     }
+    else if (round >= 7 && round <= 9) {
+        opponentCard = "Citizen";
+    }
+    else {
+        // Generate random opponent card
+        int randomNum = rand() % 100 + 1; // Generate a random number between 1 and 100
 
-    // Check the outcome of the round
+        if (kaijiSide == "Emperor") {
+            if (randomNum <= 80) {
+                opponentCard = "Citizen";
+            }
+            else {
+                opponentCard = "Slave";
+            }
+        }
+        else if (kaijiSide == "Slave") {
+            if (randomNum <= 20) {
+                opponentCard = "Emperor";
+            }
+            else {
+                opponentCard = "Citizen";
+            }
+        }
+    }
+
     if (kaijiSide == "Emperor") {
-        if (opponentCard == "Civilian") {
-            mmLeft -= 1; // Deduct 1mm from contraption
-            moneyEarned += 100000; // Earn 100,000 yen
+        if (opponentCard == "Citizen") {
+            moneyEarned += wager * 100000;
+            cout << "You win! You earned " << wager * 100000 << " yen!" << endl;
         }
         else if (opponentCard == "Slave") {
-            mmLeft += 5; // Add 5mm to contraption
+            mmLeft -= wager; // Deduct the amount wagered on loss
+            cout << "You lost! The pin will now move by " << wager << "mm." << endl;
+            cout << "DRILLING.... DRILLING...." << endl;
+            cout << "KAIJI: RAGGGGGHHHHHHHHHH" << endl;
+        }
+        else if (opponentCard == "Emperor") {
+            cout << "It's a draw! No money earned." << endl;
         }
     }
     else if (kaijiSide == "Slave") {
-        if (opponentCard == "Civilian") {
-            mmLeft -= 3; // Deduct 3mm from contraption
+        if (opponentCard == "Citizen") {
+            cout << "It's a draw! No money earned." << endl;
         }
         else if (opponentCard == "Emperor") {
-            mmLeft -= 30; // Deduct 30mm from contraption
+            mmLeft -= wager; // Deduct the amount wagered on loss
+            cout << "You lost! The pin will now move by " << wager << "mm." << endl;
+            cout << "DRILLING.... DRILLING...." << endl;
+            cout << "KAIJI: RAGGGGGHHHHHHHHHH" << endl;
         }
     }
 
     // Output the result of the round
     cout << "Round " << round << ": Kaiji's Side - " << kaijiSide << ", Opponent's Card - " << opponentCard << endl;
+    system("pause");
 }
 
 bool shouldEndGame(int round, int mmLeft, int moneyEarned) {
-    if (round == 12) {
-        if (mmLeft > 0 && moneyEarned >= 20000000) {
+    if (mmLeft <= 0) {
+        cout << "Bad Ending. You wagered a total of 30mm and lost on the last round." << endl;
+        return true;
+    }
+    else if (round == 12) {
+        if (moneyEarned >= 20000000) {
             cout << "Best Ending achieved! Congratulations!" << endl;
         }
-        else if (mmLeft > 0 && moneyEarned < 20000000) {
+        else {
             cout << "Meh Ending. You still have millimeters to spare, but didn't reach 20 million yen." << endl;
-        }
-        else if (mmLeft <= 0 && moneyEarned < 20000000) {
-            cout << "Bad Ending. You wagered a total of 30mm and lost on the last round." << endl;
         }
         return true;
     }
@@ -74,33 +104,70 @@ int main() {
     // Set the seed for random number generation
     srand(static_cast<unsigned>(time(nullptr)));
 
-    // Play each round until the game ends
     while (!shouldEndGame(round, mmLeft, moneyEarned)) {
-        cout << "Round " << round << endl;
-        cout << "Enter the number of millimeters you want to wager (remaining: " << mmLeft << "mm): ";
+        system("cls");
+        cout << "Cash: " << moneyEarned << endl;
+        cout << "Distance Left (mm): " << mmLeft << endl;
+        cout << "Round " << round << "/12" << endl;
+        cout << "Side: ";
+        if (round <= 3 || round == 7 || round == 8 || round == 9) {
+            cout << "Emperor";
+            kaijiSide = "Emperor";
+        }
+        else {
+            cout << "Slave";
+            kaijiSide = "Slave";
+        }
+        cout << endl << endl;
+
+        cout << "How many mm would you like to wager, Kaiji? ";
         int wager;
         cin >> wager;
+        system("cls");
+        cout << endl;
 
-        playRound(round, mmLeft, moneyEarned);
+        if (wager >= 1 && wager <= mmLeft) {
+            cout << "Pick a card to play..." << endl;
+            if (kaijiSide == "Emperor") {
+                cout << "[1] Emperor" << endl;
+            }
+            else if (kaijiSide == "Slave") {
+                cout << "[1] Slave" << endl;
+            }
+            cout << "[2] Citizen" << endl;
+            cout << "[3] Citizen" << endl;
+            cout << "[4] Citizen" << endl;
+            cout << "[5] Citizen" << endl;
+            cout << endl;
 
-        if (mmLeft >= wager) {
-            mmLeft -= wager;
-            if (wager > 0) {
-                if (moneyEarned >= 20000000) {
-                    cout << "Congratulations! You have already reached 20 million yen!" << endl;
+            int cardChoice;
+            cin >> cardChoice;
+            system("cls");
+            cout << endl;
+
+            if (cardChoice >= 1 && cardChoice <= 5) {
+                if (cardChoice == 1) {
+                    kaijiSide = "Emperor";
                 }
                 else {
-                    if (round % 4 == 0 || round % 4 == 1 || round % 4 == 2) {
-                        moneyEarned += wager * 100000;
-                    }
-                    else {
-                        moneyEarned += wager * 500000;
-                    }
+                    kaijiSide = "Slave";
                 }
+
+                playRound(round, mmLeft, moneyEarned, wager);
+
+                // Deduct the wagered amount from mmLeft on a loss
+                if (mmLeft < 0) {
+                    mmLeft = 0;
+                }
+            }
+            else {
+                cout << "Invalid card choice. Please enter a value between 1 and 5." << endl << endl;
+                system("pause");
             }
         }
         else {
-            cout << "Invalid wager. You do not have enough millimeters left." << endl;
+            cout << "Invalid wager. Please enter a value between 1 and " << mmLeft << "." << endl << endl;
+            system("pause");
         }
 
         round++;
